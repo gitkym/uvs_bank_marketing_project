adeb0a0496c85ede9c23e98b4cf0625b53388774
gitkym <singh.udayvikram95@gmail.com>
2023-03-08 16:46:25 +0000

    make scripts
    Former-commit-id: 2cd98792f85fa5ee99383ef71c7980ac1a069d24

---
+++ make_model.py
@@ -0,0 +1,57 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+import time as time
+import pandas as pd
+
+
+def make_model(df, X_train, y_train, X_test, y_test, model, param_grid, folds=5, scoring = 'roc_auc'):
+    '''Function to fit a model and return the best parameters and accuracy score'''
+    y = df['y']
+    df = df.drop('y', axis=1)
+
+    # Create a pipeline for categorical features
+    cat_features = df.select_dtypes(include=['object']).columns
+    cat_pipeline = Pipeline([
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+    # df_cat= cat_pipeline.fit_transform(df[cat_features])
+
+    # Create a pipeline for numerical features
+    num_features = df.select_dtypes(include=['int64', 'float64']).columns
+    num_pipeline = Pipeline([
+        ('std_scaler', StandardScaler())
+    ])
+
+    # Create a column transformer
+    preprocessor = ColumnTransformer([
+            ('cat', cat_pipeline, cat_features),
+            ('num', num_pipeline, num_features)
+])
+    # Create a pipeline for the model
+    clf = Pipeline([
+        ('preprocessor', preprocessor),
+        ('classifier', model)
+    ])
+    
+    # Create a grid search object
+    cv = StratifiedKFold(n_splits=folds, shuffle=True, random_state=42)
+    clf_grid = GridSearchCV(clf,                    # model
+                       param_grid = param_grid,   # hyperparameters
+                       scoring=scoring,        # metric for scoring
+                       cv=cv)                     # number of folds
+    
+    # Fit the model
+    start_time = time.time()
+    clf_grid.fit(X_train,y_train)
+    print("Tuned Hyperparameters :", clf_grid.best_params_)
+    print("Accuracy :",clf_grid.best_score_)
+    # print training time in minutes
+    print('Training Time : {} minutes'.format(round((time.time() - start_time)/60,3)))
+    print("Test Score :",clf_grid.score(X_test,y_test))
+    
+    # Get the results
+    df_param = pd.DataFrame(clf_grid.cv_results_)
+    
+    return clf_grid, df_param
