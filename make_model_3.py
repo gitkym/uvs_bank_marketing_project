da28303f4ab1114d4b3b725c92d317e792a5ac75
gitkym <singh.udayvikram95@gmail.com>
2023-03-12 16:31:34 +0000

    smote_trails
    Former-commit-id: 7cb84f8374f9ad93b552c218a46d9b4745bbb94d

--- make_model_3.py
+++ make_model_3.py
@@ -37,18 +37,30 @@ def make_model_nl(df, model, param_grid, test_size = 0.2, folds=5, scoring = 'ro
             ('num', num_pipeline, num_features)
     ])
 
+    
     # Create Resampling pipeline
     over = SMOTE(sampling_strategy=over_size)
-    under = RandomUnderSampler(sampling_strategy=under_size)
-    # sampler = Pipeline([('over', over), ('under', under)])
-
-    # Create a pipeline for the model
-    clf = Pipeline([
+    if under_size!=None: 
+        under = RandomUnderSampler(sampling_strategy=under_size)
+        clf = Pipeline([
         ('preprocessor', preprocessor),
         ('over', over), 
         ('under', under),
         ('classifier', model)
     ])
+    else:
+        clf = Pipeline([
+        ('preprocessor', preprocessor),
+        ('over', over), 
+        ('classifier', model)
+    ])
+
+
+    # # try using defined strategy insted of float (ratio)
+    # over = SMOTE(sampling_strategy=over_size)
+    # under = RandomUnderSampler(sampling_strategy=under_size)
+    # sampler = Pipeline([('over', over), ('under', under)])
+
     
     # Create a grid search object
     cv = StratifiedKFold(n_splits=folds, shuffle=True, random_state=42)
